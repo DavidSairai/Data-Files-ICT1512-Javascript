ef9a8564aed457ee6b77c88111c2350fee3b8600
David Sairai <dsairai@gmail.com>
2021-04-27 08:59:55 +0200

    first commit

---
+++ Chapter01/Chapter/plants.js
@@ -0,0 +1,19 @@
+/*Javascript 6th Edition
+Chapter 1
+Chapetr Case
+
+Tinley Xeriscapes
+Author: David Sairai
+Date:20 March 2021
+
+Filename: plant.js
+*/
+
+/*
+Information on available plants
+including lnk to USDA websites
+*/
+//define variables  containing img src  values
+	var blanket = "images/blanket.jpg";
+	var bluestem = "images/bluestem.jpg";
+	var rugosa = "images/rugosa.jpg";
---
+++ Temp/first.js
@@ -0,0 +1,9 @@
+/*function firstName(){
+    var hello = "Hello, my name is Farai";
+    return hello;
+}
+console.log(firstName());*/
+
+var destination = "Harare";
+var location = "Zimbabwe";
+destination = destination + " is in " + location;
