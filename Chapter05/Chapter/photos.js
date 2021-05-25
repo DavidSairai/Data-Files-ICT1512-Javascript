7ef163f6987c4cec411f66009934006217de4716
David Sairai <dsairai@gmail.com>
2021-05-25 09:35:03 +0200

    DOM and BOM

--- Chapter05/Chapter/photos.js
+++ Chapter05/Chapter/photos.js
@@ -5,7 +5,7 @@
  *    Photo gallery
  *    Variables and functions
  *    Author: David Sairai
- *    Date: 17 May 2021  
+ *    Date:   17 MAy 2021
 
  *    Filename: photos.js
  */
@@ -14,6 +14,17 @@
 
 /* global variables */
 var photoOrder = [1,2,3,4,5];
+
+/* add src values to img elements based on order specified in photoOrder arrary */
+function populateFigures(){
+   var filename;
+   var currentFig;
+   for (var i = 1; i < 4; i++);{
+      filename = "images/IMG_0" + photoOrder[i] + "sm.jpg";
+      currentFig = document.getElementsByTagName("img")[i - 1];
+      currentFig.src = filename;
+   }
+}
 
 /* shift all images one figure to the left, and change values in photoOrder array to match  */
 function rightArrow() {
@@ -44,23 +55,31 @@ function zoomFig() {
    
 }
 
-/*event listenr fo left arrow, right arrow, center figure element*/
+/*create event listeners for left arrow, right arrow and center figure element */
 function createEventListeners(){
-   var leftArrow = document.getElementById("leftarrow");
-   if(leftArrow.addEventListener) {
-      leftArrow.addEventListener("click", leftArrow, false);
-      else if(leftArrow.attachEvent){
-         leftArrow.attachEvent("onclick", leftArrow);
-      }
-   var right = document.getElementsById("rightarrow")'
-   if(righarrow.addEventListener){
-      rightArrow.addEventListener("click", rightArrow, false);
-      else if(rightArrow.attachEvent){
-         rightArrow.attachEvent("onclick", rightArrow);
-      }
+   var leftarrow = document.getElementById("leftarrow");
+   if (leftarrow.addEventListener){
+      leftarrow.addEventListener("click", leftArrow, false);
    }
+   else if (leftarrow.attachEvent){
+      leftarrow.attachEvent("onclick", leftArrow);
+   }
+   var rightarrow = document.getElementById("rightarrow");
+   if (rightarrow.addEventListener) {
+      rightarrow.addEventListener("click", rightArrow, false);
+   }
+   else if (rightarrow.attachEvent){
+      leftarrow.attachEvent("onclick", rightArrow);
+   }
+   var mainFig = document.getElementsByTagName("img")[1];
+   if (mainFig.addEventListener){
+      mainFig.addEventListener("click", zoomFig, false);
+   }else if (mainFig.attachEvent){
+      mainFig.attachEvent("onclick", zoomFig);
    }
 }
+
+
 /* create event listeners and populate image elements */
 function setUpPage() {
    createEventListeners();
